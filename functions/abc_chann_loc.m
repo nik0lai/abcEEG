85a797642b90925b62a6aafa846c9e383e02876c
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-04 21:31:37 -0400

    add chann location first draft

---
+++ functions/abc_chann_loc.m
@@ -0,0 +1,56 @@
+function [] = abc_chann_loc(setFiles, setPath, channLocPath)
+% ABC_CHANN_LOC add channels location using pop_editset() (EEGLAB).
+% 
+% Usage: abc_chann_loc(setFiles, setPath, channsPath)
+% 
+% Inputs:
+%   'setPath'       - [string] a path to the folder where .set files are to be
+%                     imported.
+%   'channLocPath'  - [string] a path to the channels location file.
+%
+% Optional inputs:
+%   'setFiles'  - [cell array] list of .set files (files have to be within 
+%                 setPath folder).
+%
+% Note: EEGLAB toolbox must be installed.
+
+%% Check min arguments
+% .set files
+if isempty(setPath)
+    error(['A path to the folder with .set files has to be set.' newline ...
+        'Your parth is: ' setPath])
+end
+
+% Path to channels location files (check if empty, if number)
+if isempty(channLocPath)
+    error(['Path to channels location file is empty.'])
+elseif ~isempty(channLocPath) && ~(exist(channLocPath, 'file') == 2)
+    error(['Channels location files does not exist.' newline ...
+        'You feed the function with the following path: ' newline ...
+        channLocPath])
+end
+
+
+
+
+
+% if setFiles are not feeded, look for them
+if isempty(setFiles)
+    setFile = dir([setPath '/*.set']);
+    setFile = {setFile.name};
+end
+
+for i = 1:size(setFile, 2)
+    currSet = setFile(i);
+    %     Read dataset
+    tempEEG = pop_loadset('filename',char(currSet), 'filepath', char(setPath));
+    %     Add channel location
+    tempEEG = pop_editset(tempEEG, 'chanlocs', channLocPath);
+    %     Save dataset
+    pop_saveset(tempEEG  , 'filename', char(currSet),'filepath', char(setPath));
+    %     Progress indicator
+    disp([num2str(i) '/' num2str(size(setFile, 2))])    
+    
+end
+
+end
