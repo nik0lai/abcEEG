39ce120864a6ab0cc8b03e9331cfab7b17a456ac
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-08 11:27:57 -0400

    check min arguments

--- functions/abc_reref.m
+++ functions/abc_reref.m
@@ -17,22 +17,36 @@ function [] = abc_reref(setFiles, setPath, reRefChann, excludeChann)
 %
 % Note: EEGLAB toolbox must be installed.
 
-% if setFiles are not feeded, look for them
-if isempty(setFiles)
-    setFile = dir([setPath '/*.set']);
-    setFile = {setFile.name};
+%% Check min arguments
+% .set files
+if isempty(setPath)
+    error(['A path to the folder with .set files has to be set.' newline ...
+        'Your parth is: ' setPath])
 end
-   
+
 for i = 1:size(setFile, 2)
     currSet = setFile(i);
-    %     Load dataset
+% New reference channel
+if isempty(reRefChann)
+    error(['A new reference channel(s) has to be set.'])
+elseif ~isempty(reRefChann) && ~isnumeric(reRefChann)
+    error(['New reference channel(s) is not a number.'])
+end
+
+% Channels to exclude
+if isempty(excludeChann)
+    error(['A new reference channel(s) has to be set.'])
+elseif ~isempty(excludeChann) && ~isnumeric(excludeChann)
+    error(['New reference channel(s) is not a number.'])
+end
+    % Load dataset
     tempEEG = pop_loadset('filename',char(currSet), 'filepath', char(setPath));
-    %     Re-reference
+    % Re-reference
     tempEEG  = pop_reref(tempEEG, reRefChann, 'exclude', excludeChann, 'keepref','on');    
-    %     Save dataset
+    % Save dataset
     pop_saveset(tempEEG  , 'filename', char(currSet),'filepath', fullfile(char(setPath)));
     
-    %     Pogress indicator
+    % Pogress indicator
     disp([num2str(i) '/' num2str(size(setFile, 2))])    
     
 end
