0ef75927d44bc8764b185f4ac465e8b9ee2c66c6
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-08 11:34:19 -0400

    fix setFiles var name and cell indexing

--- functions/abc_reref.m
+++ functions/abc_reref.m
@@ -2,15 +2,15 @@ function [] = abc_reref(setFiles, setPath, reRefChann, excludeChann)
 % ABC_REREF re-reference channels using pop_reref() (EEGLAB Toolbox)
 %
 % Usage: abc_reref(setFile, setPath, reRefChann, excludeChann)
-% 
+%
 % Inputs:
-%   'setPath'       - [string] a path to the folder where .set files are to 
+%   'setPath'       - [string] a path to the folder where .set files are to
 %                     be imported.
 %   'reRefChann'    - [integer] one or more numbers indicating new
 %                     reference channels.
 %   'excludeChann'  - [integer] one or more numbers indicating channels to
 %                     exclude.
-% 
+%
 % Optional inputs:
 %   'setFiles'  - [cell array] list of .set files (files have to be within
 %                 setPath folder).
@@ -24,8 +24,6 @@ if isempty(setPath)
         'Your parth is: ' setPath])
 end
 
-for i = 1:size(setFile, 2)
-    currSet = setFile(i);
 % New reference channel
 if isempty(reRefChann)
     error(['A new reference channel(s) has to be set.'])
@@ -42,15 +40,22 @@ end
 
 %% Files to filter
 setFiles = abc_check_files(setFiles, setPath, 'set');
+
+%% Re-reference
+for i = 1:numel(setFiles)
+    currSet = setFiles{i};
+    
     % Load dataset
-    tempEEG = pop_loadset('filename',char(currSet), 'filepath', char(setPath));
+    tmpEEG = pop_loadset('filename', currSet, 'filepath', setPath);
+    
     % Re-reference
-    tempEEG  = pop_reref(tempEEG, reRefChann, 'exclude', excludeChann, 'keepref','on');    
+    tmpEEG = pop_reref(tmpEEG, reRefChann, 'exclude', excludeChann, 'keepref','on');
+    
     % Save dataset
-    pop_saveset(tempEEG  , 'filename', char(currSet),'filepath', fullfile(char(setPath)));
+    pop_saveset(tmpEEG  , 'filename', currSet,'filepath', setPath);
     
     % Pogress indicator
-    disp([num2str(i) '/' num2str(size(setFile, 2))])    
+    disp([num2str(i) '/' num2str(numel(setFiles))])
     
 end
 
