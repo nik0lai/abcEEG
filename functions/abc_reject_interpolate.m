81b6627676c3d397887d6d278c05b1c80209b70f
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-08 20:40:35 -0400

    function wrapp and help

--- functions/abc_reject_interpolate.m
+++ functions/abc_reject_interpolate.m
@@ -1,9 +1,30 @@
-%% Reject bad components. Interpolate bad channels.
+function [] = abc_reject_interpolate(setPath, preprocPath)
+% ABC_REJECT_INTERPOLATE reject components using pop_subcomp()
+% and interpolate channels using pop_interp() (both from EEGLAB Toolbox).
+%
+% Usage: abc_reject_interpolate(setPath, preprocPath)
 
 % Read preprocessing table
 preprocTable = readtable('/home/niki/midgard/fondecyt/agustin/docs/preprocTracking.csv');
 % Keep file to run ICA with
 preprocTable = preprocTable(contains(preprocTable.toDO, 'rejectInter'), :);
+% Inputs:
+%   'setPath'       - [string] a path to the folder where .set files are to be
+%                     imported.
+%   'preprocPath'   - [string] a path to a .csv file with the following
+%                     columns (each row is a .set file). 'file_name': .set
+%                     file names, 'toDo': files that have components to
+%                     reject and channels to inteporlate should 
+%                     'rejectInter' on this column, 'duplicated_channels':
+%                     channels to interpolate (in this case, duplicated
+%                     channels), 'bad_channs': channels to interpolate for
+%                     any reason. And the following columns indicating
+%                     components to reject ('comp_eyemove', 'comp_muscle',
+%                     'comp_50hz', 'comp_ecg', 'comp_headmove',
+%                     'comp_blerp'. A filled example of this .csv file can
+%                     be found on /resources/examplePreprocTable.csv.
+% 
+% Note: EEGLAB toolbox must be installed.
 
 % Bad channels and Bad components
 badChann = cell2table(strcat(char(preprocTable{:, 'duplicated_channs'}), {','}, char(preprocTable{:, 'bad_channs'})), 'VariableNames', {'allBad'});
